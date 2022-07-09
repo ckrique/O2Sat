21d54de9a480888aa0876ffd2117f44b9b8a7fbf
Dickson Henrique Justino Daniel <ckrique@hotmail.com>
2022-07-09 01:41:49 -0300

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace O2Sat.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public class Person
+        {
+            public string name { get; set; }
+            public string surname { get; set; }
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public string Post([FromBody]Person person)
+        {
+            return "2"+ DateTime.Now.Second.ToString().Substring(1) + "." + DateTime.Now.Second.ToString() + "|" + ((DateTime.Now.Millisecond % 2 == 0) ? "ON" : "OFF");
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Scripts/O2Sat.js
@@ -0,0 +1,30 @@
+﻿var alterna = 1;
+setInterval(updateComponents, 2000);
+
+function updateComponents() {    
+    //if (alterna == 1) {
+    //    document.getElementById("pValorSaturacao").innerHTML = "29.6%";
+    //    document.getElementById("divRectangle").style.backgroundColor = "green";
+    //    alterna = 2;
+    //}
+    //else if (alterna == 2) {
+    //    document.getElementById("pValorSaturacao").innerHTML = "21.8%";
+    //    document.getElementById("divRectangle").style.backgroundColor = "orange";
+    //    alterna = 1;
+    //}
+       
+    var person = new Object();
+    person.name = "Nome";
+    person.surname = "Sobrenome";
+    $.post('http://localhost:55000/api/values', person, function (data) {
+        console.log(data);
+
+        var saturationValur = data.split("|")[0];
+        var fanExhaustorState = data.split("|")[1];
+
+        document.getElementById("pValorSaturacao").innerHTML = saturationValur;
+        document.getElementById("divRectangle").style.backgroundColor = (fanExhaustorState == "ON") ? "green" : "orange";
+    });
+
+}
+
